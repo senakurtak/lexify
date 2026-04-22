94b30db7d4c77108f418dd22cf192dfedd08cc02
Sena Kurtak <zskurtak@gmail.com>
2026-04-22 16:55:06 +0300

    feat: added reanimated & gesture library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -44,9 +45,9 @@ export default function RootLayout() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }} />
       <StatusBar style="auto" />
-    </>
+    </GestureHandlerRootView>
   );
 }
